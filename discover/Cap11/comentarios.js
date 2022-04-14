0f7a89f384f0b2e650b2f99818dabcf28b726946
felipemends <felipe.mendesv@gmail.com>
2022-04-13 23:35:19 -0300

    cap11 discovery

---
+++ discover/Cap11/comentarios.js
@@ -0,0 +1,11 @@
+// Aqui eu tenho um comentário em linha
+
+// O código abaixo escreve alguma mensagem no devtools
+console.log("Bem-vindos ao Starter")
+
+/*
+
+Já aqui tenho um comentário multi linha
+
+*/
+console.log("Bem-vindos ao Starter")
