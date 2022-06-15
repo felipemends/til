13f59ee9fc0fe25803325f92d33fe2b8156abeed
felipemends <felipe.mendesv@gmail.com>
2022-06-14 21:46:15 -0300

    NodeJS com EJS

--- discover/Cap18-NodeJS.EJS/server.js
+++ discover/Cap18-NodeJS.EJS/server.js
@@ -4,11 +4,42 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index')
+  const items = [
+    {
+      title: "D",
+      message: "esenvolver aplicações/serviços de forma fácil"
+    },
+    {
+      title: "E",
+      message: "JS usa JavaScript para renderizar HTML"
+    },
+    {
+      title: "M",
+      message: "uito fácil de usar"
+    },
+    {
+      title: "A",
+      message: "morzinho"
+    },
+    {
+      title: "I",
+      message: "nstall ejs"
+    },
+    {
+      title: "S",
+      message: "intaxe simples"
+    }
+  ]
+
+  const subtitle = 'Uma linguagem de modelagem para criação de página HTML'
+  res.render('pages/index', {
+    qualities: items,
+    subtitle: subtitle
+  })
 })
 
 app.get('/sobre', function (req, res) {
-  res.render('about')
+  res.render('pages/about')
 })
 
 app.listen(8080)
